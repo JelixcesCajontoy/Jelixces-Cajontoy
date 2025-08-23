25cad4a285cdf28a5ce774239be2ae66f291a149
JELIXCES CAJONTOY <bc.jelixces.cajontoy@cvsu.edu.ph>
2025-08-23 12:57:59 +0000

    fix this alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,13 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <Header />
-      <main className="flex-1 container mx-auto px-4 md:px-6">
-        <HeroSection />
-        <ProjectsSection />
-        <SkillsSection />
-        <TestimonialsSection />
+      <main className="flex-1">
+        <div className="container mx-auto px-4 md:px-6">
+          <HeroSection />
+          <ProjectsSection />
+          <SkillsSection />
+          <TestimonialsSection />
+        </div>
       </main>
       <Footer />
     </div>

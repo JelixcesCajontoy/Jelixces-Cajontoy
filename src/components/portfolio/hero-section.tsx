ce6a29bbb846cc9aa0bba49209cd7382bed94040
JELIXCES CAJONTOY <bc.jelixces.cajontoy@cvsu.edu.ph>
2025-10-30 03:57:48 +0000

    remove my picture in the home

--- src/components/portfolio/hero-section.tsx
+++ src/components/portfolio/hero-section.tsx
@@ -1,10 +1,8 @@
-
 "use client";
 
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ArrowDown } from "lucide-react";
-import jelixImge from '@/image/jelix.jpg';
 
 export function HeroSection() {
   const displayDescription = "A passionate and creative front-end developer with a knack for building beautiful, functional, and user-centric web applications. With a strong foundation in front-end technologies, I thrive on turning complex problems into elegant user interfaces.";
@@ -13,14 +11,6 @@ export function HeroSection() {
     <section id="home" className="w-full py-24 md:py-32 lg:py-40">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center text-center space-y-8 animate-fade-in-up">
-          <Image
-              src= {jelixImge}
-              alt="Jelixces Cajontoy Headshot"
-              width={144}
-              height={144}
-              className="rounded-full object-cover shadow-lg border-4 border-primary"
-              data-ai-hint="professional headshot"
-          />
           <div className="space-y-3">
               <h1 className="font-headline text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl text-foreground">
               Jelixces Cajontoy
